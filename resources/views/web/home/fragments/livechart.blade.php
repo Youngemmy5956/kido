22b852152ff8a9566c199df01305613578c032b9
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-06-28 12:47:50 +0100

    text added

--- resources/views/web/home/fragments/livechart.blade.php
+++ resources/views/web/home/fragments/livechart.blade.php
@@ -6,9 +6,26 @@
                     <div class="live__cart__header">
                         <h2>Real Time Progress</h2>
                     </div>
-                    <div class="chart__img mt--60">
-                        <img src="{{asset('data/images/about/cart3.png')}}" alt="chart images">
-                    </div>
+                    <div style="height: 540px;">
+                        <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+                        <script type="text/javascript">
+                        new TradingView.widget(
+                        {
+                        "autosize": true,
+                        "symbol": "COINBASE:BTCUSD",
+                        "interval": "240",
+                        "timezone": "Etc/UTC",
+                        "theme": "light",
+                        "style": "1",
+                        "locale": "en",
+                        "toolbar_bg": "#f1f3f6",
+                        "enable_publishing": false,
+                        "allow_symbol_change": true,
+                        "container_id": "tradingview_09e24"
+                       }
+                        );
+                        </script>
+                      </div>
                 </div>
             </div>
         </div>
--- resources/views/web/home/fragments/slider.blade.php
+++ resources/views/web/home/fragments/slider.blade.php
@@ -3,9 +3,12 @@
         <div class="row">
             <div class="col-lg-8 col-xl-7 col-md-12 col-sm-12 col-12 d-flex align-items-center">
                 <div class="slide__inner">
-                    <h1>Decentralized cryptocurrency is produced.</h1>
-                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have
-                        suffered alteration in some form</p>
+                    <h1>Giving you a World Class Trading Experience</h1>
+                    <p>
+
+                        Through advanced trading infrastructure, super fast withdrawals, hyper speed execution and a
+                        responsive chat support will facilitate your trading experience to enable you to embrace the
+                        opportunities presented by the market.</p>
                     <a class="slide__btn dg__btn btn--white btn--theme" href="#">Read More</a>
                 </div>
             </div>
