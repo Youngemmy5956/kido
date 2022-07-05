06d47143b51078c856ee4f119b824c25658cdfb9
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-07-05 09:57:23 +0100

    text added

--- resources/views/components/user/activation.blade.php
+++ resources/views/components/user/activation.blade.php
@@ -1,7 +1,7 @@
 <section class="pac">
     <div class="flex flex-col md:flex-row justify-evenly items-center space-x-2 w-full mb-4">
     </div>
-  
+
 </section>
 <input type="hidden" value="{{ auth('web')->user()->investment->package ?? 'no' }}" class="package">
 <script>
@@ -14,9 +14,9 @@
       method:'get'
     }).then((res) => res.json()).then((val) => {
        let data = val.map((res) => {
-        return ` 
+        return `
                             <div class="md:w-4/12 w-full flex flex-col justify-center border space-y-10 text-center">
-                                <span class="block fa fa-${res.name == package.value ? 'unlock' : 'lock'} text-5xl mt-6"></span>
+                                <span class="block fa fa-${res.name == package.value ? 'unlock' : ''} text-5xl mt-6"></span>
                                     <p class="text-3xl">${res.amount}</p>
                                     <div class="flex flex-col justify-start text-left mx-6 space-y-2">
                                     ${res.content.split(',').map((da) => {
