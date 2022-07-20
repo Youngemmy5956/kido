2e480fc3e1f9f60bcf68c9df5c065f985945d4dd
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-07-20 07:45:09 +0100

    text added

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -1,27 +1,25 @@
 @extends('layout.admin')
 
 @section('content')
+    <div class="m-4">
 
-  <div class="m-4">
+        <p>Products</p>
 
-    <p>Products</p>
 
-  
 
-    <form action="{{ route('admin.product') }}" method="post" enctype="multipart/form-data">
+        <form action="{{ route('admin.product') }}" method="post" enctype="multipart/form-data">
 
-        @csrf
+            @csrf
 
-        <input type="text" name="name" placeholder="Product Name" class="border m-2">
-        <br>
-         <input type="text" name="content" placeholder="About Product" class="border m-2">
-        <br>
-        <label>Upload product image</label>
-        <input type="file" name="file">
-        <br>
-        <input type="submit" value="upload" name="submit" class="bg-green-600 p-4 text-white">
-    </form>
+            <input type="text" name="name" placeholder="Product Name" class="border m-2">
+            <br>
+            <input type="text" name="content" placeholder="About Product" class="border m-2">
+            <br>
+            <label>Upload product image</label>
+            <input type="file" name="file">
+            <br>
+            <input type="submit" value="upload" name="submit" class="bg-green-600 p-4 text-white">
+        </form>
 
     </div>
-
 @endsection
