03e2beed343971270ae77eece115e3dfc306f0f1
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-07-26 11:03:17 +0100

    text added

--- resources/views/web/layout/includes/header.blade.php
+++ resources/views/web/layout/includes/header.blade.php
@@ -3,8 +3,8 @@
         <div class="row align-items-center">
             <div class="col-lg-2 col-xl-2 col-sm-6 col-12">
                 <div class="logo">
-                    <a href="{{ route('index') }}">
-                        <img src="{{ asset('data/images/logo/ki4.png') }}" alt="logo images">
+                    <a href="{{route("index")}}">
+                        <img src="{{asset('data/images/logo/ki4.png')}}" alt="logo images">
                     </a>
                 </div>
             </div>
@@ -12,28 +12,90 @@
                 <nav class="mainmenu__nav">
                     <ul class="mainmenu">
                         <li class="drop">
-                            <a href="{{ route('index') }}">Home</a>
+                            <a href="{{route("index")}}">Home</a>
                             <ul class="dropdown">
-
+                                {{-- <li>
+                                    <a href="{{route("web.home")}}">Home Version 01</a>
+                                </li> --}}
+                                {{-- <li>
+                                    <a href="{{route("web.home")}}">Home Version 02</a>
+                                </li> --}}
+                                {{-- <li>
+                                    <a href="{{route("web.home")}}">Home Version 03</a>
+                                </li> --}}
+                                {{-- <li>
+                                    <a href="{{route("web.home")}}">Home Version 04</a>
+                                </li> --}}
                             </ul>
                         </li>
                         <li>
-                            <a href="{{ route('about') }}">About Us</a>
+                            <a href="{{route("about")}}">About Us</a>
                         </li>
                         <li class="drop">
-                            <a href="{{ route('service') }}">Service</a>
+                            <a href="{{route("service")}}">Service</a>
                             <ul class="dropdown">
-
+                                {{-- <li>
+                                    <a href="{{route("web.service")}}">Service Page</a>
+                                </li> --}}
+                                {{-- <li>
+                                    <a href="single-service.html">Single Service</a>
+                                </li> --}}
                             </ul>
                         </li>
+                        {{-- <li>
+                            <a href="merchants.html">Merchants</a>
+                        </li> --}}
+                        {{-- <li class="drop">
+                            <a href="blog.html">News</a>
+                            <ul class="dropdown">
+                                <li>
+                                    <a href="blog.html">Blog Page</a>
+                                </li>
+                                <li>
+                                    <a href="blog-right-sidebar.html">Blog Right Sidebar</a>
+                                </li>
+                                <li>
+                                    <a href="blog-details.html">Blog Details</a>
+                                </li>
+                            </ul>
+                        </li> --}}
                         <li class="drop">
-                            <a href="{{ route('team') }}">team</a>
-
+                            <a href="{{route("team")}}">team</a>
+                            {{-- <li>
+                                <a href="{{route("web.about_btc")}}">About Coin</a>
+                            </li> --}}
+                            <li>
+                                <a href="{{route("wallet")}}">Wallet Page</a>
+                            </li>
+                            {{-- <ul class="dropdown">
+                                <li>
+                                    <a href="about-bitcoin.html">About Bitcoin</a>
+                                </li>
+                                <li>
+                                    <a href="merchants.html">Merchants</a>
+                                </li>
+                                <li>
+                                    <a href="team.html">Team Page</a>
+                                </li>
+                                <li>
+                                    <a href="wallet.html">Wallet Page</a>
+                                </li>
+                                <li>
+                                    <a href="login.html">Login</a>
+                                </li>
+                                <li>
+                                    <a href="register.html">Register</a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                                <li>
+                                    <a href="#"></a>
+                                </li>
+                            </ul>
+                        </li> --}}
                         <li>
-                            <a href="{{ route('wallet') }}">Wallet Page</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('contact') }}">Contact</a>
+                            <a href="{{route("contact")}}">Contact</a>
                         </li>
 
                     </ul>
@@ -42,10 +104,10 @@
             <div class="col-lg-2 col-xl-2 col-sm-6 col-12">
                 <ul class="accounts">
                     <li>
-                        <a href="{{ route('login') }}">Log in</a>
+                        <a href="{{route("login")}}">Log in</a>
                     </li>
                     <li class="active">
-                        <a href="{{ route('register') }}">Sign up</a>
+                        <a href="{{route("register")}}">Sign up</a>
                     </li>
                 </ul>
             </div>
@@ -56,26 +118,92 @@
             <nav class="mobilemenu__nav">
                 <ul class="meninmenu">
                     <li>
-                        <a href="{{ route('index') }}">Home</a>
+                        <a href="{{route("index")}}">Home</a>
+                        {{-- <ul>
+                            <li>
+                                <a href="index.html">Home Version 01</a>
+                            </li>
+                            <li>
+                                <a href="index-2.html">Home Version 02</a>
+                            </li>
+                            <li>
+                                <a href="index-3.html">Home Version 03</a>
+                            </li>
+                            <li>
+                                <a href="index-4.html">Home Version 04</a>
+                            </li>
+                        </ul> --}}
+                    </li>
+                    <li>
+                        <a href="{{route("about")}}">About Us</a>
+                    </li>
+                    <li>
+                        <a href="{{route("service")}}">Service</a>
+                        {{-- <ul>
+                            <li>
+                                <a href="service.html">Service Page</a>
+                            </li>
+                            <li>
+                                <a href="single-service.html">Single Service</a>
+                            </li>
+                        </ul> --}}
+                    </li>
+                    {{-- <li>
+                        <a href="merchants.html">Merchants</a>
+                    </li> --}}
+                    {{-- <li>
+                        <a href="blog.html">News</a>
+                        <ul>
+                            <li>
+                                <a href="blog.html">Blog Page</a>
+                            </li>
+                      li      <li>
+                                <a href="blog-right-sidebar.html">Blog Right Sidebar</a>
+                            </li>
+                            <li>
+                                <a href="blog-details.html">Blog Details</a>
+                            </li>
+                        </ul>
+                    </li> --}}
+                    <li>
+                        <a href="{{route("team")}}">Team</a>
+                        {{-- <li>
+                            <a href="{{route("web.about_btc")}}">About Coin</a>
+                        </li> --}}
+                        <li>
+                            <a href="{{route("wallet")}}">Wallet Page</a>
+                        </li>
 
-                    </li>
-                    <li>
-                        <a href="{{ route('about') }}">About Us</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('service') }}">Service</a>
 
+                        {{-- <ul>
+                            <li>
+                                <a href="about-bitcoin.html">About Bitcoin</a>
+                            </li>
+                            <li>
+                                <a href="merchants.html">Merchants</a>
+                            </li>
+                            <li>
+                                <a href="team.html">Team Page</a>
+                            </li>
+                            <li>
+                                <a href="wallet.html">Wallet Page</a>
+                            </li>
+                            <li>
+                                <a href="login.html">Login</a>
+                            </li>
+                            <li>
+                                <a href="register.html">Register</a>
+                            </li>
+                            <li>
+                                <a href="contact.html">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#"></a>
+                            </li>
+                        </ul> --}}
                     </li>
-
                     <li>
-                        <a href="{{ route('team') }}">Team</a>
-
-                    <li>
-                        <a href="{{ route('wallet') }}">Wallet Page</a>
-                    </li>
-                    </li>
-                    <li>
-                        <a href="{{ route('contact') }}">Contact</a>
+                        <a href="{{route("contact")}}">Contact</a>
                     </li>
                 </ul>
             </nav>
